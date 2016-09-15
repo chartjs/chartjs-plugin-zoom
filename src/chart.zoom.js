99a77a1ae17b4cb16ef0e953826b0bcb42a88159
Robert Kirk <Robert Kirk>
2016-09-15 14:24:43 +0100

    fixing indents

--- src/chart.zoom.js
+++ src/chart.zoom.js
@@ -119,11 +119,11 @@ function doZoom(chartInstance, zoom, center) {
 }
 
 function panIndexScale(scale, delta, panOptions) {
-	var labels = scale.chart.data.labels;
-	var lastLabelIndex = labels.length - 1;
+    var labels = scale.chart.data.labels;
+    var lastLabelIndex = labels.length - 1;
     var offsetAmt = Math.max((scale.ticks.length - ((scale.options.gridLines.offsetGridLines) ? 0 : 1)), 1);
     var panSpeed = panOptions.speed;
-	var minIndex = scale.minIndex;
+    var minIndex = scale.minIndex;
     var step = Math.round(scale.width / (offsetAmt * panSpeed));
     var maxIndex;
 
@@ -132,10 +132,10 @@ function panIndexScale(scale, delta, panOptions) {
     minIndex = zoomNS.panCumulativeDelta > step ? Math.max(0, minIndex -1) : zoomNS.panCumulativeDelta < -step ? Math.min(lastLabelIndex - offsetAmt + 1, minIndex + 1) : minIndex;
     zoomNS.panCumulativeDelta = minIndex !== scale.minIndex ? 0 : zoomNS.panCumulativeDelta;
 
-	maxIndex = Math.min(lastLabelIndex, minIndex + offsetAmt - 1);
+    maxIndex = Math.min(lastLabelIndex, minIndex + offsetAmt - 1);
 
-	scale.options.ticks.min = labels[minIndex];
-	scale.options.ticks.max = labels[maxIndex];
+    scale.options.ticks.min = labels[minIndex];
+    scale.options.ticks.max = labels[maxIndex];
 }
 
 function panTimeScale(scale, delta) {
