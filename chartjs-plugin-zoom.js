56b4c272f3557592efa595d893f9363ccdea434e
Ben McCann <ben@benmccann.com>
2018-01-06 10:55:19 -0800

    Relese 0.6.1

--- chartjs-plugin-zoom.js
+++ chartjs-plugin-zoom.js
@@ -1,7 +1,7 @@
 /*!
  * chartjs-plugin-zoom
  * http://chartjs.org/
- * Version: 0.6.0
+ * Version: 0.6.1
  *
  * Copyright 2016 Evert Timberg
  * Released under the MIT license
@@ -136,8 +136,17 @@ function zoomTimeScale(scale, zoom, center, zoomOptions) {
 	var minDelta = newDiff * min_percent;
 	var maxDelta = newDiff * max_percent;
 
-	options.time.min = rangeMinLimiter(zoomOptions, scale.getValueForPixel(scale.getPixelForValue(scale.min) + minDelta));
-	options.time.max = rangeMaxLimiter(zoomOptions, scale.getValueForPixel(scale.getPixelForValue(scale.max) - maxDelta));
+	var newMin = scale.getValueForPixel(scale.getPixelForValue(scale.min) + minDelta);
+	var newMax = scale.getValueForPixel(scale.getPixelForValue(scale.max) - maxDelta);
+
+	var diffMinMax = newMax.diff(newMin);
+	var minLimitExceeded = rangeMinLimiter(zoomOptions, diffMinMax) != diffMinMax;
+	var maxLimitExceeded = rangeMaxLimiter(zoomOptions, diffMinMax) != diffMinMax;
+
+	if (!minLimitExceeded && !maxLimitExceeded) {
+		options.time.min = newMin;
+		options.time.max = newMax;
+	}
 }
 
 function zoomNumericalScale(scale, zoom, center, zoomOptions) {
@@ -162,7 +171,7 @@ function zoomScale(scale, zoom, center, zoomOptions) {
 	}
 }
 
-function doZoom(chartInstance, zoom, center) {
+function doZoom(chartInstance, zoom, center, whichAxes) {
 	var ca = chartInstance.chartArea;
 	if (!center) {
 		center = {
@@ -178,11 +187,20 @@ function doZoom(chartInstance, zoom, center) {
 		var zoomMode = helpers.getValueOrDefault(chartInstance.options.zoom.mode, defaultOptions.zoom.mode);
 		zoomOptions.sensitivity = helpers.getValueOrDefault(chartInstance.options.zoom.sensitivity, defaultOptions.zoom.sensitivity);
 
+		// Which axe should be modified when figers were used.
+		if (zoomMode == 'xy' && whichAxes !== undefined) {
+			// based on fingers positions
+			_whichAxes = whichAxes;
+		} else {
+			// no effect
+			_whichAxes = 'xy';
+		}
+
 		helpers.each(chartInstance.scales, function(scale, id) {
-			if (scale.isHorizontal() && directionEnabled(zoomMode, 'x')) {
+			if (scale.isHorizontal() && directionEnabled(zoomMode, 'x') && directionEnabled(_whichAxes, 'x')) {
 				zoomOptions.scaleAxes = "x";
 				zoomScale(scale, zoom, center, zoomOptions);
-			} else if (!scale.isHorizontal() && directionEnabled(zoomMode, 'y')) {
+			} else if (!scale.isHorizontal() && directionEnabled(zoomMode, 'y') && directionEnabled(_whichAxes, 'y')) {
 				// Do Y zoom
 				zoomOptions.scaleAxes = "y";
 				zoomScale(scale, zoom, center, zoomOptions);
@@ -209,17 +227,19 @@ function panIndexScale(scale, delta, panOptions) {
 
 	maxIndex = Math.min(lastLabelIndex, minIndex + offsetAmt - 1);
 
-	// Forbids zooming behavior.
-	if (scale.maxIndex != maxIndex && scale.minIndex != minIndex) {
-		scale.options.ticks.min = rangeMinLimiter(panOptions, labels[minIndex]);
-		scale.options.ticks.max = rangeMaxLimiter(panOptions, labels[maxIndex]);
-	}
+	scale.options.ticks.min = rangeMinLimiter(panOptions, labels[minIndex]);
+	scale.options.ticks.max = rangeMaxLimiter(panOptions, labels[maxIndex]);
 }
 
 function panTimeScale(scale, delta, panOptions) {
 	var options = scale.options;
-	options.time.min = rangeMinLimiter(panOptions, scale.getValueForPixel(scale.getPixelForValue(scale.min) - delta));
-	options.time.max = rangeMaxLimiter(panOptions, scale.getValueForPixel(scale.getPixelForValue(scale.max) - delta));
+	var limitedMax = rangeMaxLimiter(panOptions, scale.getValueForPixel(scale.getPixelForValue(scale.max) - delta));
+	var limitedMin = rangeMinLimiter(panOptions, scale.getValueForPixel(scale.getPixelForValue(scale.min) - delta));
+
+	var limitedTimeDelta = delta < 0 ? limitedMax - scale.max : limitedMin - scale.min;
+
+	options.time.max = scale.max + limitedTimeDelta;
+	options.time.min = scale.min + limitedTimeDelta;
 }
 
 function panNumericalScale(scale, delta, panOptions) {
@@ -415,7 +435,34 @@ var zoomPlugin = {
 			var currentPinchScaling;
 			var handlePinch = function handlePinch(e) {
 				var diff = 1 / (currentPinchScaling) * e.scale;
-				doZoom(chartInstance, diff, e.center);
+				var rect = e.target.getBoundingClientRect();
+				var offsetX = e.center.x - rect.left;
+				var offsetY = e.center.y - rect.top;
+				var center = {
+					x : offsetX,
+					y : offsetY
+				};
+
+				// fingers position difference
+				var x = Math.abs(e.pointers[0].clientX - e.pointers[1].clientX);
+				var y = Math.abs(e.pointers[0].clientY - e.pointers[1].clientY);
+
+				// diagonal fingers will change both (xy) axes
+				var p = x / y;
+				var xy;
+				if (p > 0.3 && p < 1.7) {
+					xy = 'xy';
+				}
+				// x axis
+				else if (x > y) {
+					xy = 'x';
+				}
+				// y axis
+				else {
+					xy = 'y';
+				}
+
+				doZoom(chartInstance, diff, center, xy);
 
 				// Keep track of overall scale
 				currentPinchScaling = e.scale;
