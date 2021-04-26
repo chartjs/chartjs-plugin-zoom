4b7c818eadafbbc91adb7457863fdb9291269e3e
Jukka Kurkela <jukka.kurkela@gmail.com>
2021-04-26 21:51:47 +0300

    Add missing return types (#469)

--- types/index.d.ts
+++ types/index.d.ts
@@ -12,9 +12,9 @@ declare module 'chart.js' {
   }
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   interface Chart<TType extends keyof ChartTypeRegistry = keyof ChartTypeRegistry, TData = DistributiveArray<ChartTypeRegistry[TType]['defaultDataPoint']>, TLabel = unknown> {
+    pan(pan: PanAmount, options?: Partial<PanOptions>, scales?: Scale[]): void;
+    zoom(zoom: ZoomAmount, options?: Partial<ZoomOptions>, useTransition?: boolean): void;
     resetZoom(): void;
-    pan(pan: PanAmount, options?: Partial<PanOptions>, scales?: Scale[]);
-    zoom(zoom: ZoomAmount, options?: Partial<ZoomOptions>, useTransition?: boolean);
   }
 }
 
@@ -22,6 +22,6 @@ declare const Zoom: Plugin;
 
 export default Zoom;
 
-export function doPan(chart: Chart, pan: PanAmount, options?: Partial<PanOptions>, scales?: Scale[]);
-export function doZoom(chart: Chart, zoom: ZoomAmount, options?: Partial<ZoomOptions>, useTransition?: boolean);
-export function resetZoom(chart: Chart);
+export function doPan(chart: Chart, pan: PanAmount, options?: Partial<PanOptions>, scales?: Scale[]): void;
+export function doZoom(chart: Chart, zoom: ZoomAmount, options?: Partial<ZoomOptions>, useTransition?: boolean): void;
+export function resetZoom(chart: Chart): void;
