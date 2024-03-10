738e454ab4272039f54b701547d5f4e9a04d64c1
Bavo Coremans <bavoco@gmail.com>
2024-03-10 14:20:53 +0100

    refactor: vuepress to vitepress

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,80 +1,4 @@
-import * as path from 'path';
-import { DefaultThemeConfig, defineConfig, PluginTuple } from 'vuepress/config';
 
-const docsVersion = "VERSION";
-const base = process.env.NODE_ENV === "development" ? '/chartjs-plugin-zoom/master/' : `/chartjs-plugin-zoom/${docsVersion}/`;
-
-export default defineConfig({
-  title: 'chartjs-plugin-zoom',
-  description: 'A zoom and pan plugin for Chart.js >= 3.0.0',
-  theme: 'chartjs',
-  base,
-  dest: path.resolve(__dirname, '../../dist/docs'),
-  head: [
-    ['link', {rel: 'icon', href: '/favicon.ico'}],
-  ],
-  plugins: [
-    ['flexsearch'],
-    ['redirect', {
-      redirectors: [
-        // Default sample page when accessing /samples.
-        {base: '/samples', alternative: ['basic']},
-      ],
-    }],
-    [
-      'vuepress-plugin-typedoc',
-      {
-        entryPoints: ['../../src/index.ts'],
-        hideInPageTOC: true,
-        tsconfig: 'tsconfig.json',
-        sidebar: {
-          fullNames: true,
-          parentCategory: 'API',
-        },
-      },
-    ],
-    ['@simonbrunel/vuepress-plugin-versions', {
-      filters: {
-        suffix: (tag) => tag ? ` (${tag})` : '',
-        title: (v, vars) => window.location.href.includes('master') ? 'Development (master)' : v + (vars.tag ? ` (${vars.tag})` : ''),
-      },
-      menu: {
-        text: '{{version|title}}',
-        items: [
-          {
-            text: 'Documentation',
-            items: [
-              {
-                text: 'Development (master)',
-                link: '/chartjs-plugin-zoom/master/',
-                target: '_self',
-              },
-              {
-                type: 'versions',
-                text: '{{version}}{{tag|suffix}}',
-                link: '/chartjs-plugin-zoom/{{version}}/',
-                exclude: /^[0]\.[0-4]\./,
-                group: 'minor',
-                target: '_self',
-              }
-            ]
-          },
-          {
-            text: 'Release notes (5 latest)',
-            items: [
-              {
-                type: 'versions',
-                limit: 5,
-                target: '_blank',
-                group: 'patch',
-                link: 'https://github.com/chartjs/chartjs-plugin-zoom/releases/tag/v{{version}}'
-              }
-            ]
-          }
-        ]
-      },
-    }],
-  ] as PluginTuple[],
   chainWebpack: (config) => {
     config.module
       .rule('chart.js')
@@ -95,12 +19,6 @@ export default defineConfig({
     });
   },
   themeConfig: {
-    repo: 'chartjs/chartjs-plugin-zoom',
-    logo: '/favicon.ico',
-    lastUpdated: 'Last Updated',
-    searchPlaceholder: 'Search...',
-    editLinks: false,
-    docsDir: 'docs',
     chart: {
       imports: [
         ['scripts/register.js'],
