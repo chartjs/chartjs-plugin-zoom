846aad6115b6e9fe9cafad9b29b41dd89386e45e
Bavo Coremans <bavoco@gmail.com>
2024-03-10 14:20:53 +0100

    refactor: vuepress to vitepress

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,100 @@
+import { defineConfig } from 'vitepress'
+const docsVersion = "VERSION";
+const base = process.env.NODE_ENV === "development" ? '/chartjs-plugin-zoom/master/' : `/chartjs-plugin-zoom/${docsVersion}/`;
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "chartjs-plugin-zoom",
+  description: "A zoom and pan plugin for Chart.js >= 3.0.0",
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+  ],
+  base,
+  outDir: '../../dist/docs',
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    logo: '/hero.svg',
+    footer: {
+      message: 'Released under the MIT License',
+      copyright: 'Copyright © 2016-2024 chartjs-plugin-zoom contributors',
+    },
+    nav: [
+      { 
+        text: docsVersion,
+        items: [
+          { text: 'Development (master)', link: '/chartjs-plugin-zoom/master/' },
+          { text: '2.x.x', link: '/chartjs-plugin-zoom/2.0.1/' },
+          { text: '1.x.x', link: '/chartjs-plugin-zoom/1.3.0/' },
+        ],
+      },
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/integration' },
+      { text: 'Reference', link: '/api/README' },
+      { text: 'Samples', link: `/samples/` },
+      {
+        text: 'Ecosystem',
+        ariaLabel: 'Community Menu',
+        items: [
+          { text: 'Awesome', link: 'https://github.com/chartjs/awesome' },
+        ]
+      },
+      { text: 'GitHub', link: 'https://github.com/chartjs/chartjs-plugin-zoom' },
+    ],
+
+    sidebar: [
+      {
+        text: 'Guide',
+        collapsed: false,
+        link: '/guide/integration',
+        items: [
+          { text: 'Integration', link: '/guide/integration' },
+          { text: 'Usage', link: '/guide/usage' },
+          { text: 'Options', link: '/guide/options' },
+          { text: 'Animations', link: '/guide/animations' },
+          { text: 'Developers', link: '/guide/developers' },
+        ],
+      },
+      {
+        text: 'Samples',
+        collapsed: false,
+        link: '/samples/basic',
+        items: [
+          { text: 'Basic', link: '/samples/basic' },
+          { 
+            text: 'Wheel Zoom',
+            collapsed: true,
+            items: [
+              { text: 'Category Scale', link: '/samples/wheel/category' },
+              { text: 'Logarithmic Scale', link: '/samples/wheel/log' },
+              { text: 'Time Scale', link: '/samples/wheel/time' },
+              { text: 'Over Scale Mode', link: '/samples/wheel/over-scale-mode' },
+              { text: 'Click to Zoom', link: '/samples/wheel/click-zoom' },
+            ],
+          },
+          { 
+            text: 'Drag to Zoom',
+            collapsed: true,
+            items: [
+              { text: 'Category Scale', link: '/samples/drag/category' },
+              { text: 'Linear Scale', link: '/samples/drag/linear' },
+              { text: 'Logarithmic Scale', link: '/samples/drag/log' },
+              { text: 'Time Scale', link: '/samples/drag/time' },
+              { text: 'Timeseries Scale', link: '/samples/drag/timeseries' },
+            ],
+          },
+          { text: 'API', link: '/samples/api' },
+          { text: 'Fetch Data', link: '/samples/fetch-data' },
+          { text: 'Pan Region', link: '/samples/pan-region' },
+        ]
+      },
+      {
+        text: 'API Reference',
+        link: '/api/README',
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/chartjs/chartjs-plugin-zoom' },
+    ]
+  }
+})
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,78 +1,4 @@
-const path = require('path');
-const docsVersion = "VERSION";
-const base = process.env.NODE_ENV === "development" ? '/chartjs-plugin-zoom/master/' : `/chartjs-plugin-zoom/${docsVersion}/`;
 
-module.exports = {
-  title: 'chartjs-plugin-zoom',
-  description: 'A zoom and pan plugin for Chart.js >= 3.0.0',
-  theme: 'chartjs',
-  base,
-  dest: path.resolve(__dirname, '../../dist/docs'),
-  head: [
-    ['link', {rel: 'icon', href: '/favicon.ico'}],
-  ],
-  plugins: [
-    ['flexsearch'],
-    ['redirect', {
-      redirectors: [
-        // Default sample page when accessing /samples.
-        {base: '/samples', alternative: ['basic']},
-      ],
-    }],
-    [
-      'vuepress-plugin-typedoc',
-      {
-        entryPoints: ['../../types/index.d.ts'],
-        hideInPageTOC: true,
-        tsconfig: 'tsconfig.json',
-        sidebar: {
-          fullNames: true,
-          parentCategory: 'API',
-        },
-      },
-    ],
-    ['@simonbrunel/vuepress-plugin-versions', {
-      filters: {
-        suffix: (tag) => tag ? ` (${tag})` : '',
-        title: (v, vars) => window.location.href.includes('master') ? 'Development (master)' : v + (vars.tag ? ` (${tag})` : ''),
-      },
-      menu: {
-        text: '{{version|title}}',
-        items: [
-          {
-            text: 'Documentation',
-            items: [
-              {
-                text: 'Development (master)',
-                link: '/chartjs-plugin-zoom/master/',
-                target: '_self',
-              },
-              {
-                type: 'versions',
-                text: '{{version}}{{tag|suffix}}',
-                link: '/chartjs-plugin-zoom/{{version}}/',
-                exclude: /^[0]\.[0-4]\./,
-                group: 'minor',
-                target: '_self',
-              }
-            ]
-          },
-          {
-            text: 'Release notes (5 latest)',
-            items: [
-              {
-                type: 'versions',
-                limit: 5,
-                target: '_blank',
-                group: 'patch',
-                link: 'https://github.com/chartjs/chartjs-plugin-zoom/releases/tag/v{{version}}'
-              }
-            ]
-          }
-        ]
-      },
-    }],
-  ],
   chainWebpack: (config) => {
     config.module
       .rule('chart.js')
@@ -93,12 +19,6 @@ module.exports = {
     });
   },
   themeConfig: {
-    repo: 'chartjs/chartjs-plugin-zoom',
-    logo: '/favicon.ico',
-    lastUpdated: 'Last Updated',
-    searchPlaceholder: 'Search...',
-    editLinks: false,
-    docsDir: 'docs',
     chart: {
       imports: [
         ['scripts/register.js'],
@@ -106,54 +26,5 @@ module.exports = {
         ['scripts/utils.js', 'Utils'],
       ]
     },
-    nav: [
-      {text: 'Home', link: '/'},
-      {text: 'Guide', link: '/guide/'},
-      {text: 'API', link: '/api/'},
-      {text: 'Samples', link: `/samples/`},
-      {
-        text: 'Ecosystem',
-        ariaLabel: 'Community Menu',
-        items: [
-          { text: 'Awesome', link: 'https://github.com/chartjs/awesome' },
-        ]
-      }
-    ],
-    sidebar: {
-      '/guide/': [
-        '',
-        'integration',
-        'usage',
-        'options',
-        'animations',
-        'developers',
-      ],
-      '/samples/': [
-        'basic',
-        {
-          title: 'Wheel Zoom',
-          children: [
-            'wheel/category',
-            'wheel/log',
-            'wheel/time',
-            'wheel/over-scale-mode',
-            'wheel/click-zoom',
-          ]
-        },
-        {
-          title: 'Drag to Zoom',
-          children: [
-            'drag/category',
-            'drag/linear',
-            'drag/log',
-            'drag/time',
-            'drag/timeseries',
-          ]
-        },
-        'api',
-        'fetch-data',
-        'pan-region',
-      ],
-    }
   }
 };
